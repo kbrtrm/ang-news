c2aa0996691268d239737985e080da3e6c98946a
Kevin Bertram <kbertr@saic.edu>
2014-11-02 19:41:13 -0600

    joey

---
+++ app/scripts/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+
+/* global app:true */
+/* exported app */
+
+var app = angular
+  .module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ])
+  .constant('FIREBASE_URL', 'https://shining-inferno-517.firebaseio.com/')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function(Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl',
+        resolve: {
+          user: function(Auth) {
+            return Auth.resolveUser();
+          }
+        }
+      })
+      .when('/users/:userId', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
